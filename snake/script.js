958efc8f1f068c7f9497ade4c7fcf11e902b759d
Michelle Radomski <michi.radomski@hotmail.com>
2022-02-08 10:39:55 +0100

    Safeguard for crashing into own snake body

--- snake/script.js
+++ snake/script.js
@@ -36,21 +36,37 @@ const snakeDirection = (event) => {
     console.log(event);
     //up
     if (event.key === "w" || event.key === "ArrowUp") {
+        if (yVelocity == 1) {
+            //in case you want to move up while going down, you'll crash in own body so this is not allowed
+            return; //this will stop the function, can't move up when going down
+        }
         yVelocity = -1; //moving on y-axis: you'll move up
         xVelocity = 0; //moving on x-axis will stop cause you'll move on y-axis
     }
     //down
     if (event.key === "s" || event.key === "ArrowDown") {
+        if (yVelocity == -1) {
+            //in case moving up, we can't move down
+            return;
+        }
         yVelocity = 1;
         xVelocity = 0;
     }
     //left
     if (event.key === "a" || event.key === "ArrowLeft") {
+        if (xVelocity == 1) {
+            //in case moving right, can't move left
+            return;
+        }
         yVelocity = 0;
         xVelocity = -1;
     }
     //right
     if (event.key === "d" || event.key === "ArrowRight") {
+        if (xVelocity == -1) {
+            //in case moving left, can't move right
+            return;
+        }
         yVelocity = 0;
         xVelocity = 1;
     }
