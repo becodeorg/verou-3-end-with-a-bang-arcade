57e8001fb2e2c37093737b613c98669022a68ccc
beryld <deback.beryl@gmail.com>
2022-02-09 13:56:21 +0100

    Feat : allow ball movement and wall bumpings

--- pong/main.js
+++ pong/main.js
@@ -10,12 +10,12 @@ const info = document.getElementById("info");
 let paddleOneCoor = paddleOne.getBoundingClientRect();
 let paddleTwoCoor = paddleTwo.getBoundingClientRect();
 let fieldCoor = theField.getBoundingClientRect();
-const ballStartingCoor = ballStarting.getBoundingClientRect();
-const baballCoor = ballStartingCoor;
+const ballStartingCoor = ballStarting.getBoundingClientRect(); // Keep the ball coor when in the middle of field
+let baballCoor = ballStartingCoor;
 // need a paddle class to organise the gen position of them
-const paddleSCommon = document
-  .querySelector(".paddles")
-  .getBoundingClientRect();
+// const paddleSCommon = document
+//   .querySelector(".paddles")
+//   .getBoundingClientRect();
 
 let directionX = Math.floor(Math.random() * 4) + 3;
 let directionY = Math.floor(Math.random() * 4) + 3;
@@ -25,38 +25,132 @@ let directionYd = Math.floor(Math.random() * 2);
 document.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     gameStatus = gameStatus == "startingScreen" ? "play" : "startingScreen";
-    /* the line above is a ternary operator, if gameStatus is = starts it'll become play and opposite by pressing enter*/
+    /* the line above is a ternary operator, if gameStatus is = startingScreen it'll become play and opposite by pressing enter*/
   }
-  if (gameStatus == "play") {
+
+  if (gameStatus === "play") {
     info.textContent = "Game Started";
     requestAnimationFrame(() => {
       directionX = Math.floor(Math.random() * 4) + 3;
       directionY = Math.floor(Math.random() * 4) + 3;
       directionXd = Math.floor(Math.random() * 4) + 3;
       directionYd = Math.floor(Math.random() * 4) + 3;
-      //   letsMoveIt(directionX, directionY, directionXd, directionYd);
+        letsMoveIt(directionX, directionY, directionXd, directionYd);
     });
   }
-  if (gameStatus == "play") {
-    if (event.key == "q") {
+  if (gameStatus === "play") {
+    if (event.key === "q") {
       paddleOne.style.top =
         Math.max(
           theField.getBoundingClientRect().top,
           paddleOne.getBoundingClientRect().top - 23.9
         ) + "px";
+        paddleOneCoor = paddleOne.getBoundingClientRect()
     }
-    if (event.key == "w") {
-      paddleOne.style.top =
-        Math.min(
-          fieldCoor.bottom - paddleSCommon.height,
-          paddleOneCoor.top + window.innerHeight * 
-          0.0002
-        ) + "px";
-      paddleOneCoor = paddleOne.getBoundingClientRect();
+    if (event.key === "w") 
+        {
+          
+          paddleOne.style.top =
+          Math.min(
+            theField.getBoundingClientRect().bottom - paddleOne.getBoundingClientRect().height  ,
+            paddleOne.getBoundingClientRect().top + 23.9 
+          ) + "px";
+          paddleOneCoor = paddleOne.getBoundingClientRect()
+          
+        }
+
+    if(event.key == "ArrowUp"){
+      paddleTwo.style.top = 
+      Math.max(
+        theField.getBoundingClientRect().top,
+        paddleTwo.getBoundingClientRect().top -23.9
+      ) + "px";
+      paddleTwoCoor = paddleTwo.getBoundingClientRect();
+    }    
+    
+    if( event.key == "ArrowDown"){
+     paddleTwo.style.top =
+     Math.min(
+       fieldCoor.bottom - paddleTwo.getBoundingClientRect().height,
+       paddleTwoCoor.top + window.innerHeight * 0.1 
+     ) + "px"
+     paddleTwoCoor = paddleTwo.getBoundingClientRect()
     }
+
+    
   }
 });
 
+
+const letsMoveIt = (directionX, directionY, directionXd, directionYd) => {
+                    if( baballCoor.top <= fieldCoor.top){ // if the ball is going outside of top field
+                      directionYd = 1;
+                    }
+                    if (baballCoor.bottom >= fieldCoor.bottom){ // if the ball is goint outside of bottom field
+                      directionYd = 0;
+                    } 
+
+                    if(baballCoor.left <= paddleOneCoor.right  // if ball is further left than the paddle  + top & bottom conditions
+                      &&
+                      baballCoor.top >= paddleOneCoor.top
+                      &&
+                      baballCoor.bottom <= paddleTwoCoor.bottom){
+                        directionXd = 1;
+                        // directionX = Math.floor(Math.random() * 4) + 3;
+                        // directionY = Math.floor(Math.random() * 4) + 3;  // if those two are uncommented, the speed and direction change after touching a wall ^^
+                        }
+
+                    if (baballCoor.right >= paddleTwoCoor.left // same for paddle right
+                      && baballCoor.top >= paddleTwoCoor.top
+                      && baballCoor.bottom <= paddleTwoCoor.bottom){
+                        directionXd = 0;
+                        // directionX = Math.floor(Math.random() * 4) + 3 ;
+                        // directionY = Math.floor(Math.random() * 4) + 3 ;
+                      }    
+
+                      // score implement + restart
+                      if ( baballCoor.left <= fieldCoor.left
+                         || baballCoor.right >= fieldCoor.right
+                        ){
+                          
+                          //TODO:  put a scoring system
+                          // if ( baballCoor.left<= fieldCoor.left){
+                          //   score2.innerHTML = score2 + 1; // not sure, maybe need to add a "+" sign before the score2 ?
+                          // }
+                          // else {
+                          //   score1.innerHTML = score1 +1;
+                          // }
+                          gameStatus = "startingScreen'"
+                          baballCoor = ballStartingCoor; // here pressing enter bug ! 
+                          info.textContent = " ENTER = POOOONG "
+                          return;
+                        }
+                        //Let's make it move for fsk
+                        baball.style.top = baballCoor.top + directionY * ( directionYd == 0 ? -1 : 1) + "px"; // ternary conditions for direction
+                        baball.style.left =  baballCoor.left + directionX * ( directionXd == 0 ? -1 : 1) + "px";
+                        console.log(baball.getBoundingClientRect()) 
+                        console.log(theField.getBoundingClientRect())
+                        
+                        baballCoor = baball.getBoundingClientRect()
+                        requestAnimationFrame(() =>{
+                          letsMoveIt(directionX,directionY,directionXd,directionYd)
+                        });
+                      }
+
+
+
+
+
+
+// {
+    //   paddleOne.style.top =
+    //     Math.min(
+    //       fieldCoor.bottom - paddleSCommon.height,
+    //       paddleOneCoor.top + window.innerHeight * 
+    //       0.0002
+    //     ) + "px";
+    //   paddleOneCoor = paddleOne.getBoundingClientRect();
+    // }
 // document.addEventListener("keydown", (event) => {
 //   if (event.key === "ArrowUp") {
 //     console.log("heyUp");
